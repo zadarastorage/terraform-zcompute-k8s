fd9221e16b0c6aca97a5fea877ca7fd5d31fc340
Container <localhost>
2026-01-23 05:23:12 +0000

    chore(01-03): add commitlint pre-commit hook
    - Add .pre-commit-config.yaml with commitlint hook at commit-msg stage
    - Add commitlint.config.js extending @commitlint/config-conventional
    - Enforces Conventional Commits format validation before commit

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert']
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never']
+  }
+};
